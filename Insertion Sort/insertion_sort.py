0d8bf3f26d09ef001077d0fb36e56b3692d43788
Aki <akitee77@outlook.com>
2025-01-29 05:24:58 +0800

    Insertion Sort Implementation and Analysis
    A detailed implementation of the Insertion Sort algorithm in Python, including explanations, test cases with unittest, and an analysis of time and space complexity.

---
+++ Insertion Sort/insertion_sort.py
@@ -0,0 +1,37 @@
+"""
+Insertion Sort Steps
+1.  Consider the array as two parts: Sorted (initially containing only the first element) and Unsorted (initially containing the rest of the array).
+2.  Starting from the first number in the Unsorted portion, compare it with the numbers in the Sorted portion (to its left).
+3.  Shift larger numbers in the Sorted portion one position to the right until you find the correct position for the current number. 
+    Insert the number into its correct position.
+4.  Repeat this process for every number in the Unsorted portion until the entire array is sorted.
+"""
+def insertion_sort_function(array):
+    """
+    Sorts the array in ascending order using the insertion sort algorithm.
+   
+    Args:
+        array (list[int]): A list of integer to sort.
+
+    Returns:
+        array (list[int]): The sorted array.
+    
+    Complexity:
+        Time Complexity: O(N^2) 
+        since the array is halved at each step
+
+        Space Complexity: O(N) 
+        - input space + auxiliary space = O(N) + O(1) = O(N)
+        
+        Auxiliary Space: O(1) 
+        - Constant space needed
+
+    """
+    for i in range(1, len(array)):
+        key = array[i]
+        j = i - 1
+        while(j >= 0 and array[j] > key):
+            array[j + 1] = array[j]
+            j = j - 1
+        array[j + 1] = key
+    return array
