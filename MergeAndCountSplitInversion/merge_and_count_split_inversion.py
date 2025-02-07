0029584ead481d6ae88bcd6a280e787a9ca9967b
Aki <akitee77@outlook.com>
2025-02-08 03:27:00 +0800

    Merge and Count Split Inversion algorithm Implementation and Analysis
    A detailed implementation of the Merge and Count Split Inversion algorithm in Python, including explanations, test cases with unittest, and an analysis of time and space complexity

---
+++ MergeAndCountSplitInversion/merge_and_count_split_inversion.py
@@ -0,0 +1,79 @@
+"""
+Merge and count split inversion function is an extension of the merge algorithm.
+In addition to merging two sorted arrays, it also counts the number of inversion pairs
+that occur between the two arrays during the merge process.
+
+What is inversion pair, for example:
+An inversion pair consists of two elements where:
+- One element is larger than the other, and
+- The larger element appears before the smaller one when merging the two sorted arrays.
+
+For example:
+    A = [2, 4, 8]
+    B = [3, 4, 5]
+
+In this case:
+    - (4, 3) is an inversion pair because 4 from array A is larger than 3 from array B.
+    - (8, 3), (8, 4), and (8, 5) are also inversion pairs because 8 from array A is larger than these elements from array B.
+
+Key Insight for Counting Inversions:
+    Whenever an element in array A is greater than an element in array B during the merge process,
+    all the remaining elements in array A (after the current element) will also form inversion pairs 
+    with the current element from array B. This insight helps in counting inversions efficiently.
+
+Calculation of Inversions:
+The inversion count is updated as follows:
+splitInversions += (n1 + 1) - i
+Where:
+    - n1 is the length of array A.
+    - i is the current index in array A being processed.
+    - n1 + 1 represents the total number of elements in array A.
+    - Subtracting i ensures that we count only those elements in A that are yet to be processed 
+      and that are larger than the current element in B.
+"""
+def merge_and_count_split_inversion_function(A, B):
+    """
+    Merge two sorted array into ascending order and return the number of inversions
+
+    Parameters:
+        A (int): The first sorted array.
+        B (int): The second sorted array.
+    
+    Returns:
+        tuple(array[int], int): 
+        - array[int]: A new sorted array that contains all elements from both arrays A and B.
+        - int: the number of inversions
+
+    Complexity:
+        Time Complexity: O(n + m) 
+        n : length of array A
+        m : length of array B
+        The total number of comparisons is proportional to the total number of elements, which is n+m.
+
+        Total Space Complexity: O(n + m)  
+        input space + auxiliary space = O(n + m) + O(n + m) = O(n + m) 
+        
+        Auxiliary Space: O(n + m) 
+        Need extra space to store the merged result array
+    """
+    result = []
+    splitInversions = 0
+    i = j = 0
+    n1 = len(A) - 1
+    n2 = len(B) - 1
+
+    while i <= n1 or j <=n2:
+        if j > n2 or(i <= n1 and A[i] <= B[j]):
+            result.append(A[i])
+            i +=1
+        else:
+            result.append(B[j])
+            j +=1
+            splitInversions = splitInversions + (n1 + 1) - i
+    return (result, splitInversions)
+
+A = [2, 4, 6]
+B = [1, 3, 5]
+merged, inversions = merge_and_count_split_inversion_function(A, B)
+print("array:", merged)
+print("number of inversions pair:", inversions)
---
+++ MergeAndCountSplitInversion/test_merge_and_count_split_inversion.py
@@ -0,0 +1,37 @@
+import unittest
+from merge_and_count_split_inversion import merge_and_count_split_inversion_function
+class TestBinarySearch(unittest.TestCase):
+    def test_one_figure(self):
+        """Test case of one figure multiplication"""
+        a = [1]
+        b = [2]
+        result, inversions = merge_and_count_split_inversion_function(a, b)
+        self.assertEqual(result, [1, 2])  
+        self.assertEqual(inversions, 0)
+
+    def test_two_figure(self):
+        """Test case of two figure multiplication"""
+        a = [1, 2]
+        b = [3, 4]
+        result, inversions = merge_and_count_split_inversion_function(a, b)
+        self.assertEqual(result, [1, 2, 3, 4])   
+        self.assertEqual(inversions, 0)
+
+    def test_one_pair_same_figure(self):
+        """Test case of three figure multiplication"""
+        a = [1, 2, 8]
+        b = [2, 3, 7]
+        result, inversions = merge_and_count_split_inversion_function(a, b)
+        self.assertEqual(result, [1, 2, 2, 3, 7, 8])
+        self.assertEqual(inversions, 3) #(8, 2), (8, 3), (8, 7)
+        
+    def test_two_pair_same_figure(self):
+        """Test case of four figure multiplication"""
+        a = [1, 5, 8, 9]
+        b = [5, 6, 7, 8]
+        result, inversions = merge_and_count_split_inversion_function(a, b)
+        self.assertEqual(result, [1, 5, 5, 6, 7, 8, 8, 9])
+        self.assertEqual(inversions, 7) #(8, 5), (8, 6), (8, 7), (9, 5), (9, 6), (9, 7), (9, 8)
+
+if __name__ == "__main__":
+    unittest.main()
